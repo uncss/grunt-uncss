a54bbda85ba81d354b4a34fdf6ed2ae969b3d219
nadir-hussain <60210929+nadir-hussain@users.noreply.github.com>
2020-10-12 11:23:12 +0530

    Update uncss.js

--- tasks/uncss.js
+++ tasks/uncss.js
@@ -2,7 +2,7 @@
  * grunt-uncss
  * https://github.com/uncss/grunt-uncss
  *
- * Copyright (c) 2020 Addy Osmani
+ * Copyright (c) 2019 Addy Osmani
  * Licensed under the MIT license.
  */
 
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
         const options = this.options({
             report: 'min'
         });
-
+        let fileInProcess = 0;
         this.files.forEach(file => {
             const src = file.src.filter(filepath => {
                 if (/^https?:\/\//.test(filepath)) {
@@ -40,7 +40,12 @@ module.exports = function (grunt) {
             }
 
             try {
+                fileInProcess++;
                 uncss(src, options, (error, output, report) => {
+                    fileInProcess--;
+                    if(fileInProcess==0){
+                        done();
+                    }
                     if (error) {
                         throw error;
                     }
@@ -51,8 +56,6 @@ module.exports = function (grunt) {
                     if (typeof options.reportFile !== 'undefined' && options.reportFile.length > 0) {
                         grunt.file.write(options.reportFile, JSON.stringify(report));
                     }
-
-                    done();
                 });
             } catch (error) {
                 const err = new Error('Uncss failed.');
